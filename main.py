f575059527a444e63056266ce33e8d8ea822aef9
Tanmay Kshirsagar <112453068+tanmayk26@users.noreply.github.com>
2023-03-25 16:33:46 -0400

    Added data transformation code
    Differencing to make data stationary

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ print(f'NA value: \n{df.isna().sum()}')
 # As we can see there are no null values
 
 # Plotting dependent variable vs time
+plt.figure(figsize=(20, 14))
 plt.plot(list(df.index.values), df['pollution'])
 plt.xlabel('Time')
 plt.ylabel('Pollution')
@@ -51,9 +52,36 @@ X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df['polluti
 print(f'Training set size: {len(X_train)} rows and {len(X_train.columns)+1} columns')
 print(f'Testing set size: {len(X_test)} rows and {len(X_test.columns)+1} columns')
 
+# Stationarity Tests on raw data
 toolkit.ADF_Cal(df['pollution'])
 toolkit.kpss_test(df['pollution'])
 toolkit.CalRollingMeanVarGraph(df, 'pollution')
+
+# Transforming data to make it stationary
+df['diff_order_1'] = toolkit.differencing(df['pollution'], 1)
+
+# Stationarity Tests on transformed data
+toolkit.CalRollingMeanVarGraph(df[1:], 'diff_order_1')
+print('ADF test on diff_order_1:-')
+toolkit.ADF_Cal(df['diff_order_1'][1:])
+print('KPSS test on diff_order_1:-')
+toolkit.kpss_test(df['diff_order_1'][1:])
+
+# ACF Plot on transformed data
+toolkit.Cal_autocorr_plot(df['diff_order_1'][1:], lags=20)
+
+# Transforming data to make it stationary
+df['diff_order_2'] = toolkit.differencing(df['diff_order_1'], 2)
+
+# Stationarity Tests on transformed data
+toolkit.CalRollingMeanVarGraph(df[2:], 'diff_order_2')
+print('ADF test on diff_order_2:-')
+toolkit.ADF_Cal(df['diff_order_2'][2:])
+print('KPSS test on diff_order_2:-')
+toolkit.kpss_test(df['diff_order_2'][2:])
+
+# ACF Plot on transformed data
+toolkit.Cal_autocorr_plot(df['diff_order_2'][2:], lags=20)
 
 
 
